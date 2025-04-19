7c51ccba50503291f67ffff005d88f169b0f2f1a
William <63299223+William-eng@users.noreply.github.com>
2025-04-19 13:00:34 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class TaskResult(models.Model):
     task_id = models.CharField(max_length=255, unique=True)
-    email = models.EmailField()
-    message = models.TextField()
-    status = models.CharField(max_length=50, default='PENDING')
+    status = models.CharField(max_length=50)
+    result = models.JSONField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
